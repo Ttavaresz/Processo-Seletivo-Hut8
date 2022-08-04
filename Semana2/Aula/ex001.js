421b3e386a4c9d1f2a75baedf7e037c75b4058c2
Lucas Tavares <lucasdutratavares@gmail.com>
2022-08-04 12:17:04 -0300

    Aulas semana 2

---
+++ Semana2/Aula/ex001.js
@@ -0,0 +1,4 @@
+// 1. Imprima no console 15 vezes a frase ‘Formação Otterwise’.
+for (let i = 0; i < 15; i++) {
+    console.log("Formação Otterwise");
+}
---
+++ Semana2/Aula/ex002.js
@@ -0,0 +1,7 @@
+// 1. Crie um algoritmo que imprime todos os valores de um array qualquer, menos oultimo elemento.
+
+let array = [0, 1, 2, 1, 0];
+
+for (let i = 0; i < array.length - 1; i++) {
+    console.log(array[i])
+}
