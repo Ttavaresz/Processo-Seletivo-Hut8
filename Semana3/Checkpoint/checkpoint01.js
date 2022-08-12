e36226404ac6942200924de6fc43c9afdd4f415d
Lucas Tavares <lucasdutratavares@gmail.com>
2022-08-12 12:54:03 -0300

    Checkpoint da semana 3

---
+++ Semana3/Checkpoint/checkpoint01.js
@@ -0,0 +1,6 @@
+// 1.Crie uma função que recebe duas notas como argumento e retorna
+// a média entre elas. Utilize arrow function e atribua a uma constante.
+
+const media = (valueOne, valueTwo) => (valueOne + valueTwo)/2;
+
+console.log(media);
